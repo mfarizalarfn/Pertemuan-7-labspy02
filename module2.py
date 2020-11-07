45481347e03aa99dd277f478a23856fa7559e5d3
Mohamad Farizal Arifin <farizalarifin4777@gmail.com>
2020-11-07 09:25:26 +0700

    update README.md

---
+++ module2.py
@@ -0,0 +1,16 @@
+#membuat programe module 2
+print("Masukkan Pilihan Angka ke-1 : ")
+xangka1 = int(input())
+print("Masukkan Pilihan Angka ke-2 : ")
+xangka2 = int(input())
+print("Masukkan Pilihan Angka ke-3 : ")
+xangka3 = int(input())
+
+if (xangka1 > xangka2) and (xangka1 > xangka3):
+    print(f"Bilangan Pertama ({xangka1}) lebih besar dari Bilangan kedua dan ketiga")
+elif (xangka2 > xangka1) and (xangka2 > xangka3):
+    print(f"Bilangan kedua ({xangka2}) lebih besar dari Bilangan pertama dan ketiga")
+elif (xangka1 == xangka2) and (xangka1 == xangka3) and (xangka2 == xangka3):
+    print("Bilangan yang dimasukkan sama besar")
+else:
+    print(f"Bilangan ketiga ({xangka3}) lebih besar dari Bilangan pertama dan kedua")
